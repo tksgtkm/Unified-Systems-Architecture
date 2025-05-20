8f4753d817d64796aaef265f9187a5e18c66eac5
tksgtkm <h9cd342fd7jgx36@gmail.com>
2025-05-20 23:58:18 +0900

    main関数のデバッグ

--- c_tutorial/filemanager/main.c
+++ c_tutorial/filemanager/main.c
@@ -5,16 +5,19 @@
 #include <sys/stat.h>
 #include "utils.h"
 
+#define FILENAME "/var/notes"
+
 void usage(char *prog_name, char *filename) { 
    printf("使用方法： %s <%sに追加するデータ>\n", prog_name, filename); 
    exit(0);
 }
 
-void write_method(int userid, int fd, char *buffer, char *datafile) {
-
-    buffer = (char *)ec_malloc(100);
-    datafile = (char *)ec_malloc(20);
-    strcpy(datafile, "/var/notes");
+void write_method(int userid, char *note) {
+    int fd;
+    char *buffer = (char *)ec_malloc(100);
+    char *datafile = (char *)ec_malloc(20);
+    strcpy(datafile, FILENAME);
+    strcpy(buffer, note);
 
     printf("[DEBUG] buffer @ %p: \'%s\'\n", buffer, buffer);
     printf("[DEBUG] datafile @ %p: \'%s\'\n", datafile, datafile);
@@ -65,28 +68,79 @@ int find_user_note(int fd, int user_uid) {
     return length;
 }
 
-int read_method(int fd, int uid, char *searchstring) {
+int search_note(char *note, char *keyword) {
+    int i, keyword_length, match=0;
+
+    keyword_length = strlen(keyword);
+    if (keyword_length == 0)
+        return 1;
+
+    for (i=0; i<strlen(note); i++) {
+        if (note[i] == keyword[match]) {
+            match++;
+        } else {
+            if (note[i] == keyword[0])
+                match = 1;
+            else
+                match = 0;
+        }
+        if (match == keyword_length)
+            return 1;
+    }
+    return 0;
+}
+
+int print_notes(int fd, int uid, char *searchstring) {
     int note_length;
+    char byte=0, note_buffer[100];
+
+    note_length = find_user_note(fd, uid);
+    if (note_length == -1)
+        return 0;
+
+    read(fd, note_buffer, note_length);
+    note_buffer[note_length] = 0;
+
+    if (search_note(note_buffer, searchstring))
+        printf("%s", note_buffer);
+
+    return 1;
+}
+
+void read_method(int uid, char *searchstring) {
+    int note_length, fd;
+    int printing = 1;
     char byte = 0, note_buffer[100];
 
+    fd = open(FILENAME, O_RDONLY);
+    if (fd == -1)
+        fatal("main()関数内、ファイルの読み込みオープンでエラーが発生しました。");
 
+    while (printing)
+        printing = print_notes(fd, uid, searchstring);
+
+    printf("------[メモの終わり]------\n");
+
+    close(fd);
 }
 
 int main(int argc, char *argv[]) {
     int userid, fd;
+    char searchstring[100];
 
     userid = getuid();
 
-    // if (argc < 2)
-    //     usage(argv[0], datafile);
+    if (argc < 2)
+        usage(argv[0], FILENAME);
 
-    if (argv[1] == '-w') {
-        char *buffer, *datafile;
-        strcpy(buffer, argv[2]);
-        write_method(userid, fd, buffer, datafile);
-    } else if (argv[1] == '-r') {
-        char searchstring[100];
-        strcpy(searchstring, argv[2]);
+    if (strcmp(argv[1], "-w") == 0) {
+        write_method(userid, argv[2]);
+    } else if (strcmp(argv[1], "-r") == 0) {
+        if (argc > 2)
+            strcpy(searchstring, argv[2]);
+        else
+            searchstring[0] = 0;
 
+        read_method(userid, searchstring);
     }
 }
